c03055720e70b858d42ebec80d8e8dd7a48a49c6
Josh <joshuamace@gmail.com>
2024-02-21 12:58:51 -0600

    finished setting up the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
+import './App.css';
+import ContactList from './components/ContactList';
 
 function App() {
-  
-
   return (
     <>
-      
+      <ContactList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
